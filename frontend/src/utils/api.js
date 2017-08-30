4116c4963a7317f42904af40a61893d4dc2eb07d
José Naves Moura Neto <josenaves@gmail.com>
2017-08-30 09:07:58 -0300

    Add fetchPosts method

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -9,3 +9,17 @@ export const fetchCategories = () => {
     )
     .catch(err => console.error(err))
 }
+
+export const fetchPosts = () => {
+  const headers = new Headers();
+  headers.append('Authorization', 'xcx')
+  const options = { method: 'get', headers }
+
+  return fetch('http://localhost:5001/posts', options)
+    .then(
+      res => {
+        return res.json()
+      }
+    )
+    .catch(err => console.error(err))
+}
