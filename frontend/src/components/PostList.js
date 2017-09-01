64a60c31eca36c0da07c3feea520ef8abb130e03
José Naves Moura Neto <josenaves@gmail.com>
2017-08-31 21:09:28 -0300

    Add the rest of post fields on screen

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -4,7 +4,13 @@ const PostList = ({ posts }) => {
   return (
     <div>
       { posts.map((p) =>
-      <p key={p.id}> {p.title} </p>
+      <div key={p.id}>
+        <p><b>{p.title}</b></p>
+        <p>Score: {p.category}</p>
+        <p>Auhtor: {p.author}</p>
+        <p>Score: {p.voteScore}</p>
+        <p>{p.body}</p>
+      </div>
       )}
     </div>
   );
