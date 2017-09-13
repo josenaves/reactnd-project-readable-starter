6d1274d43c531b85b1e55752dd96fb0bc0c65d6b
José Naves Moura Neto <josenaves@gmail.com>
2017-09-12 21:03:43 -0300

    Remove debuging logs

--- backend/server.js
+++ backend/server.js
@@ -209,7 +209,6 @@ app.delete('/posts/:id', (req, res) => {
 })
 
 app.post('/posts/:id', bodyParser.json(), (req, res) => {
-    console.log("body", req.body)
     const { option } = req.body
     const id = req.params.id
     posts.vote(req.token, id, option)
